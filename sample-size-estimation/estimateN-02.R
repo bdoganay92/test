b3163739ffd2b389bec3d139b7eaadb58197ee65
Jamie Yap <jamieyap@users.noreply.github.com>
2020-01-11 13:12:02 -0500

    Update results.

--- sample-size-estimation/estimateN-02.R
+++ sample-size-estimation/estimateN-02.R
@@ -32,7 +32,7 @@ source(file.path(path.code,"input-utils.R"))
 source(file.path(path.code,"datagen-utils.R"))
 source(file.path(path.code,"analysis-utils.R"))
 
-load(file.path(path.output_data, use.working.corr, "estimated-covmat-01.RData"))
+load(file.path(path.output_data, use.working.corr, "estimated-covmat-02.RData"))
 path.code <- Sys.getenv("path.code")  # Need to change paths back
 path.input_data <- Sys.getenv("path.input_data")   # Need to change paths back
 path.output_data <- Sys.getenv("path.output_data")   # Need to change paths back
@@ -43,25 +43,21 @@ for(idx.i in 1:length(collect.estimates)){
   
   # Calculate DeltaQ
   idx.input.means <- collect.estimates[[idx.i]]$input.means
-  input.means <- list.input.means[[idx.input.means]]
+  input.means <- auc.list.input.means[[idx.input.means]]
   source(file.path(path.code, "calc-truth.R"))
   
   # Calculate N.required
   z.eta <- qnorm(1-input.power)
   z.alpha <- qnorm(input.alpha/2)
-  const.eos.means <- ((z.eta + z.alpha)/diff.eos.means.plusplus.minusplus)^2
-  const.change.score <- ((z.eta + z.alpha)/diff.change.score.plusplus.minusplus)^2
-  N.required.eos.means <- const.eos.means * collect.estimates[[idx.i]]$mean.sandwich.eos.means
-  N.required.change.score <- const.change.score * collect.estimates[[idx.i]]$mean.sandwich.change.score
+  const.AUC <- ((z.eta + z.alpha)/diff.AUC.plusplus.minusplus)^2
+  N.required.AUC <- const.AUC * collect.estimates[[idx.i]]$mean.sandwich.AUC
   
   tmp <- data.frame(N.large = collect.estimates[[idx.i]]$N,
                     rho = collect.estimates[[idx.i]]$rho,
                     sim = collect.estimates[[idx.i]]$sim,
                     input.means = collect.estimates[[idx.i]]$input.means,
-                    N.required.eos.means = N.required.eos.means,
-                    N.required.change.score = N.required.change.score,
-                    D.eos.means = diff.eos.means.plusplus.minusplus,
-                    D.change.score = diff.change.score.plusplus.minusplus)
+                    N.required.AUC = N.required.AUC,
+                    D.AUC = diff.AUC.plusplus.minusplus)
   collect.N <- append(collect.N, list(tmp))
 }
 
@@ -70,7 +66,7 @@ df.collect.N <- bind_rows(collect.N)
 ###############################################################################
 # Get estimated tau_mean
 ###############################################################################
-load(file.path(path.output_data, use.working.corr, "params-curve-01.RData"))
+load(file.path(path.output_data, use.working.corr, "params-curve-02.RData"))
 df.collect.correlation <- bind_rows(collect.correlation)
 df.collect.correlation <- df.collect.correlation %>% 
   group_by(datagen.params.N, datagen.params.rho, idx.input.means) %>%
@@ -81,23 +77,13 @@ df.collect.correlation <- df.collect.correlation %>%
 ###############################################################################
 this.plotdat <- df.collect.N
 
-# Get plots for end-of-study means
+# Get plots for AUC
 this.plotdat$rho <- as.factor(this.plotdat$rho)
-gg <- ggplot(this.plotdat, aes(x=D.eos.means, y=N.required.eos.means, color=rho, shape=rho))
-gg <- gg + scale_x_continuous(limits = c(min(this.plotdat$D.eos.means), max(this.plotdat$D.eos.means)))
-gg <- gg + scale_y_continuous(limits = c(min(this.plotdat$N.required.eos.means), max(this.plotdat$N.required.eos.means)))
+gg <- ggplot(this.plotdat, aes(x=D.AUC, y=N.required.AUC, color=rho, shape=rho))
+gg <- gg + scale_x_continuous(limits = c(min(this.plotdat$D.AUC), max(this.plotdat$D.AUC)))
+gg <- gg + scale_y_continuous(limits = c(min(this.plotdat$N.required.AUC), max(this.plotdat$N.required.AUC)))
 gg <- gg + geom_line(size=1.2)
 gg <- gg + geom_point(show.legend = TRUE, size=3, alpha=0.7)
-gg <- gg + labs(title = "End-of-study Means") + xlab("DELTA: Difference between DTRs (+1,+1) and (-1,+1)") + ylab("N required")
-gg
-
-# Get plots for change score
-this.plotdat$rho <- as.factor(this.plotdat$rho)
-gg <- ggplot(this.plotdat, aes(x=D.change.score, y=N.required.change.score, color=rho, shape=rho))
-gg <- gg + scale_x_continuous(limits = c(min(this.plotdat$D.change.score), max(this.plotdat$D.change.score)))
-gg <- gg + scale_y_continuous(limits = c(min(this.plotdat$N.required.change.score), max(this.plotdat$N.required.change.score)))
-gg <- gg + geom_line(size=1.2)
-gg <- gg + geom_point(show.legend = TRUE, size=3, alpha=0.7)
-gg <- gg + labs(title = "Change Score") + xlab("DELTA: Difference between DTRs (+1,+1) and (-1,+1)") + ylab("N required")
+gg <- gg + labs(title = "AUC") + xlab("DELTA: Difference between DTRs (+1,+1) and (-1,+1)") + ylab("N required")
 gg
 
